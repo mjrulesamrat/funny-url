56825adc6f5f1298b38c0027c8b6316ab327d4de
Jay Modi <mjrulesamrat@gmail.com>
2019-09-15 22:59:35 +0530

    Django version bump and example settings

--- funny_url/settings/__init__.py
+++ funny_url/settings/__init__.py
@@ -2,7 +2,7 @@ try:
     from .production import *
 except ImportError:
     try:
-        from .dev import *
+        from .local import *
     except ImportError as e:
         e.args = tuple(
             ['%s (did you create a copy settings/production-example.py?)' % e.args[0]])
---
+++ funny_url/settings/example-production.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'myprivatekeythatyourebnevergoingtoguess'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
